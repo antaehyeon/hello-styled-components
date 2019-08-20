e20ea43457a58ffb34922c022ada0a3939511d87
Hyeon <antaehyeon@live.co.kr>
2019-08-21 02:34:46 +0900

    Feat: Extra Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -8,56 +8,36 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const App = () => {
-  return (
-    <Container>
-      <GlobalStyle />
-      <Button danger>DANGER</Button>
-      <Button success>SUCCESS</Button>
-      <Anchor href="http://google.com">Go to google</Anchor>
-    </Container>
-  );
-};
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  width: 200px;
+  border: none;
+  ${awesomeCard}
+`;
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${awesomeCard}
   background-color: #ecf0f1;
 `;
 
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 180px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  $:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#c0392b" : "#1abc9c")};
-  ${props => {
-    if (props.danger) {
-      return css`
-        animation: ${rotation} 2s linear infinite;
-      `;
-    }
-  }}
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+const App = () => {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Input placeholder="please input text" />
+    </Container>
+  );
+};
 
 export default App;
