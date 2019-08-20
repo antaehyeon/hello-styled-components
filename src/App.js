0bfebff40bf1e9aa61fa6708fba8e2c5e3089aa9
Hyeon <antaehyeon@live.co.kr>
2019-08-21 01:53:54 +0900

    Feat: Styled-components 설치 및 코드 변경(styled-components화)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Fragment } from "react";
-import "./App.css";
+import React from "react";
+import styled from "styled-components";
 
 const App = () => {
   return (
-    <Fragment>
-      {/* <button className="button button--success">SUCCESS</button>
-      <button className="button button--danger">DANGER</button> */}
-      <Button danger />
-      <Button />
-    </Fragment>
+    <Container>
+      <Button danger>DANGER</Button>
+      <Button success>SUCCESS</Button>
+    </Container>
   );
 };
 
-const Button = ({ danger = false }) => (
-  <button className={danger ? "button button--danger" : "button button--success"}>{danger ? "DANGER" : "SUCCESS"}</button>
-);
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #ecf0f1;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 180px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  $:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#c0392b" : "#1abc9c")};
+`;
 
 export default App;
